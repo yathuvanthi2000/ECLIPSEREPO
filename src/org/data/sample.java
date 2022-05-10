76f373a6cbaaffbaf02c3a4c80b0c255e9bf59b3
yathuvanthi2000 <yathuvanthi.s@capgemini.com>
2022-05-10 16:32:52 +0530

    1st commit

---
+++ src/org/data/sample.java
@@ -0,0 +1,26 @@
+package org.data;
+
+public class sample {
+	
+	public static void main (String[] args) {
+			byte age =30;
+			System.out.println (age) ;
+			short id=1245;
+			System.out.println(id);
+			int accNum=345356;
+			System.out.println (accNum) ;
+			long phno= 9766453643l;
+			System. out.println (phno);
+			float avg=78.657647f;
+			System. out.println(avg);
+			double billNo=78.131131664678596;
+			System.out.println(billNo);
+			char gender ='M';
+			System.out.println ("gender:"+gender);
+			String name ="Arun";
+			System.out.println ("name: "+name) ;
+			boolean info = true;
+			System.out.println ("above info are: " +info);
+
+}
+}
